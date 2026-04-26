0ede40147e79e4b76342934e496d96f56eb83b51
ceepiyaz-hub <ceepiyaz@gmail.com>
2026-04-26 18:16:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -96,9 +96,8 @@ def save_to_google_sheet(booking):
         return True
 
     except Exception as e:
-    return {
-        "google_error": str(e)
-    }
+    print(f"GOOGLE SHEET SAVE ERROR: {str(e)}")
+    return False
 
 
 # ---------------- LOAD DATABASE ----------------
